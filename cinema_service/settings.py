cca6b1d2dd7591d58724fff1869e8e81018393aa
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-10 08:48:20 +0200

    Added secret key to the env variables

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_15vlo-g!2qf%75084q)j6nnvgxa&=*2jldexypdjw%tp)2wsw"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
